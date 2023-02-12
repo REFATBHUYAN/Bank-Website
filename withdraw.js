ab102e28d121a1a6fb1cace9c903562eb2b5cefd
REFATBHUYAN <refatbubt@gmail.com>
2023-02-12 21:04:28 +0600

    second commit

--- withdraw.js
+++ withdraw.js
@@ -1,9 +1,18 @@
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawAmount = getInputValueById('withdraw-field');
+    if(isNaN(newWithdrawAmount)){
+        alert('please enter number amount');
+        return;
+    }
+    const previousBalanceTotal = getElementValueById('balance-previous');
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('baap er bank e tk nai ato');
+        return;
+    }
     const previousWithdrawTotal = getElementValueById('withdraw-previous');
     const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    setTextElementValueById('withdraw-previous', newWithdrawAmount);
-    const previousBalanceTotal = getElementValueById('balance-previous');
+    setTextElementValueById('withdraw-previous', newWithdrawTotal);
+    
     const newBalance = previousBalanceTotal - newWithdrawAmount;
     setTextElementValueById('balance-previous', newBalance);
     
